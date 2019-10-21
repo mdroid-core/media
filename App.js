7ac3e22d89249c3cba6bb5e69270b9e47f248208
Quinn <qcasey1@gmail.com>
2019-10-21 13:18:31 -0700

    intitial redesign

--- App.js
+++ App.js
@@ -2,16 +2,27 @@ import React, {Component} from 'react';
 import {Platform, StatusBar, StyleSheet, View} from 'react-native';
 import MediaScreen from './components/MediaScreen.js';
 import {mediaHost, mainHost} from './config.json'
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
+import Swiper from 'react-native-swiper';
 
 global.mediaControllerHost = mediaHost; // Bluetooth A2DP sink running MDroid-Media or MDroid-Core
 global.mainControllerHost = mainHost; // MDroid-Core or another webserver (for album artwork)
 
 export default class App extends React.Component {
   render() {
+    changeNavigationBarColor('#000000', false);
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" backgroundColor="#000000" translucent={true} />
-        <MediaScreen />
+        <Swiper
+					index={0}
+          horizontal={false}
+          loop={false}
+					style={mainStyles.swiperContainer}
+					showsPagination={false}>
+          <MediaScreen />
+        </Swiper>
       </View>
     );
   }
